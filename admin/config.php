d9d98a6403e04ac685ca315779453c3d246bd2af
YazhouMiao <1191178138@qq.com>
2014-12-09 01:13:36 +0800

    opencart 2.0.1.1 src

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ibaiwei/admin/');
+define('HTTP_CATALOG', 'http://localhost/ibaiwei/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ibaiwei/admin/');
+define('HTTPS_CATALOG', 'http://localhost/ibaiwei/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\phpStudy\WWW\ibaiwei/admin/');
+define('DIR_SYSTEM', 'D:\phpStudy\WWW\ibaiwei/system/');
+define('DIR_LANGUAGE', 'D:\phpStudy\WWW\ibaiwei/admin/language/');
+define('DIR_TEMPLATE', 'D:\phpStudy\WWW\ibaiwei/admin/view/template/');
+define('DIR_CONFIG', 'D:\phpStudy\WWW\ibaiwei/system/config/');
+define('DIR_IMAGE', 'D:\phpStudy\WWW\ibaiwei/image/');
+define('DIR_CACHE', 'D:\phpStudy\WWW\ibaiwei/system/cache/');
+define('DIR_DOWNLOAD', 'D:\phpStudy\WWW\ibaiwei/system/download/');
+define('DIR_UPLOAD', 'D:\phpStudy\WWW\ibaiwei/system/upload/');
+define('DIR_LOGS', 'D:\phpStudy\WWW\ibaiwei/system/logs/');
+define('DIR_MODIFICATION', 'D:\phpStudy\WWW\ibaiwei/system/modification/');
+define('DIR_CATALOG', 'D:\phpStudy\WWW\ibaiwei/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'ibaiwei');
+define('DB_PREFIX', 'ibw_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ibaiwei/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ibaiwei/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\phpStudy\WWW\ibaiwei/catalog/');
+define('DIR_SYSTEM', 'D:\phpStudy\WWW\ibaiwei/system/');
+define('DIR_LANGUAGE', 'D:\phpStudy\WWW\ibaiwei/catalog/language/');
+define('DIR_TEMPLATE', 'D:\phpStudy\WWW\ibaiwei/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\phpStudy\WWW\ibaiwei/system/config/');
+define('DIR_IMAGE', 'D:\phpStudy\WWW\ibaiwei/image/');
+define('DIR_CACHE', 'D:\phpStudy\WWW\ibaiwei/system/cache/');
+define('DIR_DOWNLOAD', 'D:\phpStudy\WWW\ibaiwei/system/download/');
+define('DIR_UPLOAD', 'D:\phpStudy\WWW\ibaiwei/system/upload/');
+define('DIR_MODIFICATION', 'D:\phpStudy\WWW\ibaiwei/system/modification/');
+define('DIR_LOGS', 'D:\phpStudy\WWW\ibaiwei/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'ibaiwei');
+define('DB_PREFIX', 'ibw_');
